ecc46bbea6e12d2f731d40ab39bf0299a5b926ac
MishaKolosovskyi <57897820+MishaKolosovskyi@users.noreply.github.com>
2020-04-01 15:28:30 +0300

    Update ReviewController.java

--- .gitignore/src/main/java/com/task/controller/ReviewController.java
+++ .gitignore/src/main/java/com/task/controller/ReviewController.java
@@ -1,11 +1,11 @@
 package com.task.controller;
 
 import com.task.service.ReviewService;
+import com.task.service.TextService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -13,22 +13,27 @@ import java.util.List;
 public class ReviewController {
 
     private final ReviewService reviewService;
-    private final int mostCommentedFoodLimit = 1000;
-    private final int mostActiveUsersLimit = 1000;
-    private final int mostUsedWordsLimit = 1000;
+    private final TextService textService;
 
     @Autowired
-    public ReviewController(ReviewService reviewService) {
+    public ReviewController(ReviewService reviewService, TextService textService) {
         this.reviewService = reviewService;
+        this.textService = textService;
     }
 
     @GetMapping("/most-active-users/get")
     public List<String> getMostActiveUsers(){
-        return reviewService.getMostActiveUsers(mostActiveUsersLimit);
+        return reviewService.getMostActiveUsers(100);
     }
 
     @GetMapping("/most-commented-food/get")
     public List<String> getMostCommentedFood(){
-        return reviewService.getMostCommentedFood(mostCommentedFoodLimit);
+        return reviewService.getMostCommentedFood(100);
+    }
+
+    @GetMapping("/most-used-words")
+    public List<String> getMostUsedWords(){
+        List<String> text = reviewService.getText();
+        return textService.getMostUsedWords(10, text);
     }
 }
